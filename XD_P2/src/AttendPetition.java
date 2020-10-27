f92f794c16ed250b023e571992c2db59385786fe
Julen <Julen@192.168.1.148>
2020-10-27 16:41:36 +0100

    Creacion del proyecto

---
+++ XD_P2/src/AttendPetition.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class AttendPetition extends Thread{
+	Socket connectionSocket;
+	
+	public AttendPetition(Socket s){
+		this.connectionSocket=s;
+	}
+	
+	public void run(){
+		String clientSentence;
+		String capitalizedSentence;
+		BufferedReader inFromClient;
+		
+		try {
+			inFromClient = new BufferedReader(
+				new InputStreamReader(
+						connectionSocket.getInputStream()
+					)
+			);
+			DataOutputStream outToClient = 
+					new DataOutputStream(
+							connectionSocket.getOutputStream()
+						);
+			
+			clientSentence = inFromClient.readLine();
+			
+			System.out.println("Received: " + clientSentence);
+			
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			
+			outToClient.writeBytes(capitalizedSentence);
+			
+			this.connectionSocket.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+}
---
+++ XD_P2/src/TCPServerMultiThreading.java
@@ -0,0 +1,14 @@
+import java.net.*;
+
+class TCPServerMultiThreading {
+	public static void main(String argv[]) throws Exception {
+		
+		
+		ServerSocket welcomeSocket = new ServerSocket(1234);
+
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			new AttendPetition(connectionSocket).start();
+		}
+	}
+}
